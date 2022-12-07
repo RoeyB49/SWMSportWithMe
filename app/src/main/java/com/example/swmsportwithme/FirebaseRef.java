522e5d6ee08da5055c15157fd046f453baf643f4
Tamar Revazishvili <tamar.d154@gmail.com>
2022-12-07 12:49:17 +0200

    hobbies

---
+++ app/src/main/java/com/example/swmsportwithme/FirebaseRef.java
@@ -0,0 +1,40 @@
+package com.example.swmsportwithme;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+public class FirebaseRef {
+    protected FirebaseFirestore db;
+
+    public FirebaseRef() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void addUser(Map<String, Object> user, String collectionPath) {
+        db.collection(collectionPath)
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+}
