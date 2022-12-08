e591741fb118d25b23ec9248f4b1e2d43357bd3c
Roey Biton <roey345@gmail.com>
2022-12-08 17:19:38 +0200

    Merge pull request #1 from RoeyB49/Tamar
    Tamar

--- app/src/main/java/com/example/swmsportwithme/FirebaseRef.java
+++ app/src/main/java/com/example/swmsportwithme/FirebaseRef.java
@@ -1,40 +1,54 @@
 package com.example.swmsportwithme;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.Map;
+
+
 
 public class FirebaseRef {
     protected FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
     public FirebaseRef() {
         db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void addUser(Map<String, Object> user, String collectionPath) {
-        db.collection(collectionPath)
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+    public void addUser(Map<String, Object> user, String collectionPath,String UID) {
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("capital", true);
+//
+//        db.collection("cities").document("BJ")
+//                .set(data, SetOptions.merge());
+
+//        db.collection(collectionPath)
+//                .document(UID).set(user)
+//                .addOnSuccessListener(new <DocumentReference>()) {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
     }
 
 }
