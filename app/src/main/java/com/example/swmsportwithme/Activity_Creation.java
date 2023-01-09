456a5a1808088c020fd915efda8e2d7933c5d3b1
Tamar Revazishvili <tamar.d154@gmail.com>
2023-01-09 09:51:02 +0200

    Improved date and time in activity creation

--- app/src/main/java/com/example/swmsportwithme/Activity_Creation.java
+++ app/src/main/java/com/example/swmsportwithme/Activity_Creation.java
@@ -1,23 +1,22 @@
 package com.example.swmsportwithme;
 
-import static com.example.swmsportwithme.Registration.DATE_FORMAT;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -25,9 +24,13 @@ import java.util.Objects;
 public class Activity_Creation extends AppCompatActivity {
     private String[] activities;
     private Spinner activitiesSpinner;
-    EditText date, time;
+    //    EditText time;
     FirebaseRef db = new FirebaseRef();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    // on below line we are creating variables.
+    private Button date;
+    private Button time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,74 @@ public class Activity_Creation extends AppCompatActivity {
         activitiesSpinner = (Spinner) findViewById(R.id.Type_of_activity);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, activities);
         activitiesSpinner.setAdapter(adapter);
+
+        // Date Picker
+        date = findViewById(R.id.idBtnPickDate);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        Activity_Creation.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int selectedYear,
+                                                  int selectedMonth, int selectedDay) {
+                                // on below line we are setting date to our button.
+                                if (selectedYear >= year && selectedMonth >= month && selectedDay >= day) {
+                                    date.setText(selectedDay + "-" + (selectedMonth + 1) + "-" + selectedYear);
+                                }
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+            }
+        });
+
+        // Time Picker
+        time = findViewById(R.id.idBtnPickTime);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting the
+                // instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting our hour, minute.
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+                // on below line we are initializing our Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(Activity_Creation.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int selectedHour,
+                                                  int selectedMinute) {
+                                // on below line we are setting selected time
+                                // in our text view.
+                                time.setText(selectedHour + ":" + selectedMinute);
+                            }
+                        }, hour, minute, true);
+                // at last we are calling show to
+                // display our time picker dialog.
+                timePickerDialog.show();
+            }
+        });
 
 
         Button confirm = (Button) findViewById(R.id.host_create_activity_confirm);
@@ -71,37 +142,28 @@ public class Activity_Creation extends AppCompatActivity {
         }
         return true;
     }
-    public static boolean isDateValid(String date)
-    {
-        try {
-            DateFormat df = new SimpleDateFormat(DATE_FORMAT);
-            df.setLenient(false);
-            df.parse(date);
-            return true;
-        } catch (ParseException e) {
-            return false;
-        }
-    }
-    private boolean validatedate(){
-        date = findViewById(R.id.host_activity_date);
-        String val = date.getText().toString().trim();
-        if (val.isEmpty()) {
-            date.setError("Field can not be empty");
-            return false;
-        } else if(!isDateValid(val)){
-            date.setError("Birth date is not valid!");
-            return false;
-        }else
-            return true;
 
-    }
-    private boolean validateTime() {
-        time = findViewById(R.id.host_activity_time);
-        String val = time.getText().toString().trim();
-        if (val.isEmpty()) {
-            time.setError("Field can not be empty");
+    private boolean validatedate() {
+        String val = date.getText().toString().trim();
+        if (val.equals("Pick Date")) {
+            date.setError("Please choose date");
+            Toast.makeText(this, "Please choose date", Toast.LENGTH_LONG).show();
             return false;
         } else {
+            date.setError(null);
+            return true;
+        }
+
+    }
+
+    private boolean validateTime() {
+        String val = time.getText().toString().trim();
+        if (val.equals("Pick Time")) {
+            time.setError("Please choose time");
+            Toast.makeText(this, "Please choose time", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            time.setError(null);
             return true;
         }
     }
